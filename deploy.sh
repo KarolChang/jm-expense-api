49172469ab9d13fab3bde4f3ab8f45db5dda7069
KarolChang <super000999888@gmail.com>
2022-05-11 14:03:18 +0800

    fix: heroku config

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# abort on errors (遇到錯誤就中止)
+# set -e
+
+# build
+# npm run build
+
+# push to firebase
+# firebase deploy --project=default
+
+# git
+# git init
+# git add .
+# git commit -m 'feat: line unlink'
+
+# push to Gitlab
+# git push origin master
+
+# echo
+# echo "🚀 JM Expense is running on https://jm-expense-2022.firebaseapp.com/"
