3f2af9961acbe815ea1faf7a891553e06d021594
KarolChang <super000999888@gmail.com>
2022-05-11 16:24:34 +0800

    fix

--- src/app.ts
+++ src/app.ts
@@ -13,16 +13,15 @@ async function main() {
   if (process.env.NODE_ENV === 'production') {
     let config: ConnectionOptions = {
       type: 'mysql',
-      url: process.env.CLEARDB_DATABASE_URL,
-      synchronize: true,
-      logging: true,
-      entities: ['dist/graphql/entity/**/*.js']
+      // synchronize: true,
+      // logging: true,
+      // entities: ['dist/graphql/entity/**/*.js']
+      url: process.env.CLEARDB_DATABASE_URL
     }
-    Object.assign(config, {
-      // url: process.env.CLEARDB_DATABASE_URL,
-      // entities: ['dist/graphql/entity/**/*.js'],
-      seeds: ['dist/graphql/seed/*.js']
-    })
+    // Object.assign(config, {
+    //   url: process.env.CLEARDB_DATABASE_URL,
+    //   seeds: ['dist/graphql/seed/*.js']
+    // })
     await createConnection(config)
   } else {
     await createConnection()
@@ -33,8 +32,9 @@ async function main() {
   const schema = await buildSchema({
     resolvers:
       process.env.NODE_ENV === 'production'
-        ? ([path.resolve('./dist/graphql/entity/**/index.js')] as NonEmptyArray<string>)
+        ? ([path.resolve('./dist/graphql/entity/**/index.{js, ts}')] as NonEmptyArray<string>)
         : ([path.resolve('./src/graphql/entity/**/index.ts')] as NonEmptyArray<string>),
+    // ([path.resolve('./dist/graphql/entity/**/index{.js,.ts}')] as NonEmptyArray<string>)
     dateScalarMode: 'isoDate', // 預設是 'isoDate'
     nullableByDefault: true,
     validate: false
