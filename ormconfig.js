0c1f928b8bb2f00f4423c711fe5b840e840b0067
KarolChang <super000999888@gmail.com>
2022-05-25 10:56:02 +0800

    package.json

--- ormconfig.js
+++ ormconfig.js
@@ -6,7 +6,7 @@ module.exports = {
   entities: ['dist/graphql/entity/**/*.js'],
   seeds: ['dist/graphql/seed/*.js'],
   subscribers: ['dist/graphql/entity/BasicListener.js', 'dist/graphql/entity/**/*.listener.js'],
-  migrations: ['dist/graphql/migrations/*.ts'],
+  migrations: ['dist/graphql/migrations/*.js'],
   cli: {
     migrationsDir: 'dist/graphql/migrations'
     // entitiesDir: '',
