cf5190625fe7726693de5ac0c0645d802889da75
KarolChang <super000999888@gmail.com>
2022-06-13 16:03:50 +0800

    refactor: schedule controller

--- src/routings/scheuleController.ts
+++ src/routings/scheuleController.ts
@@ -1,14 +1,20 @@
 import { JsonController, Get } from 'routing-controllers'
 import schedule from 'node-schedule'
+import { CustomJob } from '@/graphql/CustomJob'
 
 @JsonController()
 export class ScheduleController {
   @Get('/schedule')
   async getJobs() {
     const jobs = schedule.scheduledJobs
+    const results = []
+    for (const val of Object.values(jobs)) {
+      const job = val as CustomJob
+      results.push({ uid: job.uid, name: job.name })
+    }
     return {
-      jobs,
-      length: Object.keys(jobs).length
+      results,
+      length: results.length
     }
   }
 }
