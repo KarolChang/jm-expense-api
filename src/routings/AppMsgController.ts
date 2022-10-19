ad462a69d71ca361c7ada6deeb6e983e988d092b
KarolChang <karolchang@damaiapp.com.tw>
2022-10-19 12:14:02 +0800

    fix: deploy to github

--- src/routings/AppMsgController.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Res } from 'routing-controllers'
-
-@Controller()
-export class BasicController {
-  @Get('/')
-  async basic(@Res() res: any) {
-    return res.send('Hello! Here is JM-Expense-Graphql API~')
-  }
-}
